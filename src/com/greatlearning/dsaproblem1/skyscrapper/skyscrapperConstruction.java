8f4e4a4d2c6a32d5e2b00b6e2fcd8c23b280c574
Tarangini S Shetty <tarangini.shetty@gmail.com>
2022-05-08 18:23:26 +0530

    Solution for Problem 1

--- src/com/greatlearning/dsaproblem1/skyscrapper/skyscrapperConstruction.java
+++ src/com/greatlearning/dsaproblem1/skyscrapper/skyscrapperConstruction.java
@@ -20,28 +20,26 @@ public class skyscrapperConstruction {
 			int element = itr.next();
 			// if element in queue is equal to floors remaining, 
 			// then print/remove the elements from stack until the difference in each element in stack is 1
-            if ( element == noOfFloorsremaining)
-            {
-            	stack.add(element);
-            	int elementPrinted = 0;
-            	System.out.println("\nDay:" + day);
-            	while (!stack.empty() && (elementPrinted == 0 || elementPrinted - stack.peek() == 1)) 
-            	{
-            		elementPrinted = stack.pop();
-            		System.out.print(elementPrinted + " ");
-            		noOfFloorsremaining = noOfFloorsremaining - 1;
-            		
-            	}
-            	
-            }
-            // if element in queue not equal to floors, then keep adding the element to stack
-            else 
-            {
-            	stack.add(element);
-            	System.out.println("\nDay:" + day);
-            }
-            // keep incrementing the day
-            day  = day + 1;   
-        }
+			if ( element == noOfFloorsremaining)
+			{
+				stack.add(element);
+				int elementPrinted = 0;
+				System.out.println("\nDay:" + day);
+				while (!stack.empty() && (elementPrinted == 0 || elementPrinted - stack.peek() == 1)) 
+				{
+					elementPrinted = stack.pop();
+					System.out.print(elementPrinted + " ");
+					noOfFloorsremaining = noOfFloorsremaining - 1;
+				}
+			}
+			// if element in queue not equal to floors, then keep adding the element to stack
+			else 
+			{
+				stack.add(element);
+				System.out.println("\nDay:" + day);
+			}
+			// keep incrementing the day
+			day  = day + 1;   
+		}
 	}
 }
