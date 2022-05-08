36cf330f278406a49aae126e592bf36da65a0f1d
Tarangini S Shetty <tarangini.shetty@gmail.com>
2022-05-08 18:14:08 +0530

    Solution for Problem 1

---
+++ src/com/greatlearning/dsaproblem1/driver/driver.java
@@ -0,0 +1,35 @@
+package com.greatlearning.dsaproblem1.driver;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+import com.greatlearning.dsaproblem1.skyscrapper.skyscrapperConstruction;
+
+public class driver {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		Queue<Integer> floorSizeQueue = new LinkedList<>();
+		skyscrapperConstruction sk = new skyscrapperConstruction();
+		
+		//Get the input
+		System.out.println("enter the total no of floors in the building");
+		int noOfFloors = sc.nextInt();
+		int floorSize;
+		for (int i = 1; i <= noOfFloors; i++)
+		{
+			System.out.println("enter the floor size given on day : "+i);
+			floorSize = sc.nextInt();
+			floorSizeQueue.add(floorSize);
+			
+		}
+		
+		sk.orderOfConstruction(noOfFloors, floorSizeQueue);
+		
+
+	}
+
+}
---
+++ src/com/greatlearning/dsaproblem1/skyscrapper/skyscrapperConstruction.java
@@ -0,0 +1,50 @@
+package com.greatlearning.dsaproblem1.skyscrapper;
+
+import java.util.Iterator;
+import java.util.Queue;
+import java.util.Stack;
+
+public class skyscrapperConstruction {
+	
+	public void orderOfConstruction(int noOfFloors, Queue<Integer> floorSizeQueue)
+	{
+		
+		//Initialization
+		int noOfFloorsremaining = noOfFloors;
+		Iterator<Integer> itr = floorSizeQueue.iterator();
+		int day = 1;
+		Stack<Integer> stack = new Stack<Integer>();
+		
+		//traverse through the queue
+		while (itr.hasNext())
+        {
+			
+			int element = itr.next();
+			// if element in queue is equal to floors remaining, then print/remove the elements from stack until the difference in each element in stack is 1
+            if ( element == noOfFloorsremaining)
+            {
+            	stack.add(element);
+            	int elementPrinted = 0;
+            	System.out.println("\nDay:" + day);
+            	while (!stack.empty() && (elementPrinted == 0 || elementPrinted - stack.peek() == 1)) {
+            		elementPrinted = stack.pop();
+            		System.out.print(elementPrinted + " ");
+            		noOfFloorsremaining = noOfFloorsremaining - 1;
+            		
+            	}
+            	
+            }
+            // if element in queue not equal to floors, then keep adding the element to stack
+            else 
+            {
+            	stack.add(element);
+            	System.out.println("\nDay:" + day);
+            }
+            // keep incrementing the day
+            day  = day + 1;
+            
+        }
+	}
+	
+
+}
