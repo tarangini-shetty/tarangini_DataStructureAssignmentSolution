bdccb350240e128468457206fc508b895026cc32
Tarangini S Shetty <tarangini.shetty@gmail.com>
2022-05-08 18:24:43 +0530

    Solution for Problem 1

--- src/com/greatlearning/dsaproblem1/driver/driver.java
+++ src/com/greatlearning/dsaproblem1/driver/driver.java
@@ -3,14 +3,11 @@ package com.greatlearning.dsaproblem1.driver;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
-
 import com.greatlearning.dsaproblem1.skyscrapper.skyscrapperConstruction;
 
 public class driver {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
+	public static void main(String[] args) {		
 		Scanner sc = new Scanner(System.in);
 		Queue<Integer> floorSizeQueue = new LinkedList<>();
 		skyscrapperConstruction sk = new skyscrapperConstruction();
@@ -23,13 +20,11 @@ public class driver {
 		{
 			System.out.println("enter the floor size given on day : "+i);
 			floorSize = sc.nextInt();
+			// Add the floorsize to queue
 			floorSizeQueue.add(floorSize);
 			
 		}
-		
-		sk.orderOfConstruction(noOfFloors, floorSizeQueue);
-		
-
+		// Find the order of construction
+		sk.orderOfConstruction(noOfFloors, floorSizeQueue);	
 	}
-
 }
